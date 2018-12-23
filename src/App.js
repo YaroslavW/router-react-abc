a2994447e18a3c4b608e997195c77cd6724a1105
YaroslavW <doctor355@yahoo.com>
2018-12-23 11:46:01 +0300

    Split application into components.

--- src/App.js
+++ src/App.js
@@ -3,27 +3,12 @@ import React, { Component } from 'react';
 import { BrowserRouter, Route, Link } from 'react-router-dom';
 import createBrowserHistory from 'history/createBrowserHistory';
 
+import Home from './components/Home';
+import About from './components/About';
+import Contacts from './components/Contacts';
+import Blog from './components/Blog';
+
 const history = createBrowserHistory();
-
-const Home = () => (
-  <div>
-    <h2>Home</h2>
-  </div>
-);
-
-const About = () => (
-  <div>
-    <h2>About</h2>
-  </div>
-);
-
-const Contacts = () => (
-  <div>
-    <h2>Contacts</h2>
-  </div>
-);
-
-
 
 class Menu extends Component {
   render() {
@@ -34,11 +19,13 @@ class Menu extends Component {
             <li><Link to="/">Home</Link></li>
             <li><Link to="/about">About</Link></li>
             <li><Link to="/contacts">Contacts</Link></li>
+            <li><Link to="/blog">Blog</Link></li>
           </ul>
           <hr />
           <Route exact path="/" component={Home} />
           <Route exact path="/about" component={About} />
           <Route path="/contacts" component={Contacts} />
+          <Route path="/blog" component={Blog} />
         </div>
       </BrowserRouter>
     );
